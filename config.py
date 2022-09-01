6f6989e775f755ae8454da006cb5d1fd7924ce38
ChookJeongChee <98305682+ChookJeongChee@users.noreply.github.com>
2022-09-01 14:47:08 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cmha16tma8pz.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chookjeongchee-employee"
+customregion = "us-east-1"
